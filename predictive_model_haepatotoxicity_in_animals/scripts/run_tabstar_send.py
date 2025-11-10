5aba5ce11355ab89dbe4d7f4b63ba718cbe6aa0c
cmarakello <cjmarkello@gmail.com>
2025-11-09 16:33:15 -0800

    Attempting retrain implementation, in progress

--- predictive_model_haepatotoxicity_in_animals/scripts/run_tabstar_send.py
+++ predictive_model_haepatotoxicity_in_animals/scripts/run_tabstar_send.py
@@ -12,11 +12,12 @@ import sys
 import os
 import pandas as pd
 import subprocess
+import zipfile
+import shutil
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from tabstar.tabstar_model import TabSTARClassifier
 from pandas import DataFrame, Series
-from sklearn.model_selection import train_test_split
 
 
 def parse_args():
@@ -57,32 +58,86 @@ def parse_args():
 
     return parser.parse_args()
 
-def extract_data(training_zip_file, testing_zip_file, training_data_labels, testing_data_labels):
+def get_directory_names(path):
+    """
+    Returns a list of directory names within the specified path.
+    """
+    directory_names = []
+    for entry in os.listdir(path):
+        full_path = os.path.join(path, entry)
+        if os.path.isdir(full_path):
+            directory_names.append(entry)
+    return directory_names
+
+#def split_train_test_data(training_zip_file, training_data_labels):
+#    cwd = os.getcwd()
+#
+#    training_data_basename = os.path.splitext(os.path.basename(training_zip_file))[0]
+#    df_training_labels = pd.read_csv(f'{cwd}/{training_data_labels}')
+#
+#    with zipfile.ZipFile(f'{cwd}/{training_zip_file}', 'r') as zip_ref:
+#        zip_ref.extractall(f'{cwd}/{training_data_basename}')
+#
+#    all_sample_ids = get_directory_names(f'{cwd}/{training_data_basename}')
+#
+#    split
+#
+#
+#    train_sample_ids = get_directory_names(f'{cwd}/output/val')
+#    train_sample_df = df_training_labels[df_training_labels['STUDYID'].isin(train_sample_ids)]
+#
+#    test_sample_ids = get_directory_names(f'{cwd}/output/test')
+#    test_sample_df = df_training_labels[df_training_labels['STUDYID'].isin(test_sample_ids)]
+#
+#
+#    shutil.make_archive(f'{cwd}/train_data', "zip", f'{cwd}/output/train')
+#    shutil.make_archive(f'{cwd}/test_data', "zip", f'{cwd}/output/val')
+#
+#    shutil.rmtree(f'{cwd}/output/val')
+#    shutil.rmtree(f'{cwd}/output/test')
+#
+#    train_sample_df.to_csv(f'{cwd}/train_label.csv', index=False)
+#    test_sample_df.to_csv(f'{cwd}/test_label.csv', index=False)
+#
+#    return 'train_data.zip', 'test_data.zip', 'train_label.csv', 'test_label.csv'
+
+def extract_data(training_zip_file, training_data_labels, testing_zip_file = None, testing_data_labels = None):
     cwd = os.getcwd()
     print(f"DEBUG CWD: {cwd}")
-    retcode = subprocess.check_call(['Rscript', 'send_data_extraction.R',
+    data_extraction_cmd = ['Rscript', f'{cwd}/example_input_app_data/send_data_extraction.R',
                                     '--training_zip_file', f'{cwd}/{training_zip_file}',
-                                    '--testing_zip_file', f'{cwd}/{testing_zip_file}',
                                     '--training_data_labels', f'{cwd}/{training_data_labels}',
-                                    '--testing_data_labels', f'{cwd}/{testing_data_labels}',
-                                    '--output_dir', f'{cwd}/test_out'], cwd=cwd)
+                                    '--output_dir', f'{cwd}/test_out']
+    if testing_zip_file is not None and testing_data_labels is not None:
+        data_extraction_cmd += ['--testing_zip_file', f'{cwd}/{testing_zip_file}', '--testing_data_labels', f'{cwd}/{testing_data_labels}']
+
+    retcode = subprocess.check_call(data_extraction_cmd, cwd=cwd)
 
     if retcode != 0:
         raise Exception(f"Data extraction failed: {retcode}")
 
     training_df = pd.read_csv(f'{cwd}/test_out/training_data.csv')
-    testing_df = pd.read_csv(f'{cwd}/test_out/testing_data.csv')
+    output_df_list = [training_df]
+    if testing_zip_file is not None or testing_data_labels is not None:
+        testing_df = pd.read_csv(f'{cwd}/test_out/testing_data.csv')
+        output_df_list += [testing_df]
 
-    return training_df, testing_df
+    return output_df_list
 
 #TODO: Implement
-def train_tabstar_model(training_df, testing_df):
+def train_tabstar_model(train_test_df_list):
 
-    x_train = training_df.iloc[:, 1:]
+    x_train = train_test_df_list[0].iloc[:, 1:]
     y_train = x_train.pop('Target_Organ')
     is_cls = True
-    x_test = testing_df.iloc[:, 1:]
-    y_test = testing_df.pop('Target_Organ')
+
+    x_test = None
+    y_test = None
+    if len(train_test_df_list) == 2:
+        x_test = train_test_df_list[1].iloc[:, 1:]
+        y_test = x_test.pop('Target_Organ')
+        print(f'DEBUG x_test: {x_test}')
+        print(f'DEBUG y_test: {y_test}')
 
     # Sanity checks
     assert isinstance(x_train, DataFrame), "x should be a pandas DataFrame"
@@ -91,17 +146,20 @@ def train_tabstar_model(training_df, testing_df):
 
     if x_test is None:
         assert y_test is None, "If x_test is None, y_test must also be None"
+        print(f'DEBUG splitting test data from training data')
         x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.1)
 
     assert isinstance(x_test, DataFrame), "x_test should be a pandas DataFrame"
     assert isinstance(y_test, Series), "y_test should be a pandas Series"
 
-    tabstar_cls = TabSTARClassifier if is_cls else TabSTARRegressor
-    tabstar = tabstar_cls()
-    tabstar.fit(x_train, y_train)
-    tabstar.save("my_model_path.pkl")
-    tabstar = TabSTARClassifier.load("my_model_path.pkl")
-    tabstar.fit(x_train, y_train)
+    #tabstar_cls = TabSTARClassifier if is_cls else TabSTARRegressor
+    #tabstar = tabstar_cls()
+    #tabstar.fit(x_train, y_train)
+    #tabstar.save("my_model_path.FDA.pkl")
+
+    #tabstar = TabSTARClassifier.load("my_model_path.FDA.pkl")
+    #tabstar.fit(x_train, y_train)
+
     y_pred = tabstar.predict(x_test)
     print(f"Test prediction: {y_pred}")
     print(f"Test truth: {y_test}")
@@ -113,15 +171,12 @@ def main():
     """Main script logic."""
     args = parse_args()
 
-    #training_df, testing_df = extract_data(args.training_zip,
-    #                                                     args.testing_zip,
-    #                                                     args.training_labels,
-    #                                                     args.testing_labels)
+    train_test_df_list = extract_data(args.training_zip,
+                                           args.training_labels,
+                                           args.testing_zip,
+                                           args.testing_labels)
 
-    cwd = os.getcwd()
-    training_df = pd.read_csv(f'{cwd}/test_out/training_data.csv')
-    testing_df = pd.read_csv(f'{cwd}/test_out/testing_data.csv')
-    train_tabstar_model(training_df, testing_df)
+    train_tabstar_model(train_test_df_list)
     #TODO: Align the testing_data target label type
 
     try:
--- predictive_model_haepatotoxicity_in_animals/scripts/send_data_extraction.R
+++ predictive_model_haepatotoxicity_in_animals/scripts/send_data_extraction.R
@@ -37,42 +37,55 @@ option_list <- list(
               help = "Path to the output CSV files", metavar = "character")
 )
 # # Create OptionParser object
- opt_parser <- OptionParser(option_list = option_list)
+opt_parser <- OptionParser(option_list = option_list)
 
- # Parse command-line arguments (using "opt_parser" or simulated `args`)
- opt <- parse_args(opt_parser)
+# Parse command-line arguments (using "opt_parser" or simulated `args`)
+opt <- parse_args(opt_parser)
 
 # Set the zip file and output file path from command line arguments
 training_zip_file <- opt$training_zip_file
-testing_zip_file <- opt$testing_zip_file
+
 training_data_labels<- opt$training_data_labels
+
+print("DEBUG: flag1")
+
+testing_zip_file <- opt$testing_zip_file
 testing_data_labels <- opt$testing_data_labels
+print("DEBUG: flag2")
 output_dir <- opt$output_dir
 
 # Debug prints
 
 cat("Training ZIP:", training_zip_file, "\n")
 cat("Training CSV:", training_data_labels, "\n")
-cat("Testing ZIP:", testing_zip_file, "\n")
-cat("Testing CSV:", testing_data_labels, "\n")
+print("DEBUG: flag3")
+if (!is.null(testing_zip_file)) {
+    cat("Testing ZIP:", testing_zip_file, "\n")
+}
+if (!is.null(testing_data_labels)) {
+    cat("Testing CSV:", testing_data_labels, "\n")
+}
+print("DEBUG: flag4")
 
 # Check that all input files exist
 stopifnot(file.exists(training_zip_file))
-stopifnot(file.exists(testing_zip_file))
 stopifnot(file.exists(training_data_labels))
-stopifnot(file.exists(testing_data_labels))
 
 # Create a base directory for all extracted files
 temp_dir_base <- paste(output_dir, tempfile(), sep="")
-dir.create(temp_dir_base, recursive = TRUE)
+dir.create(temp_dir_base, mode = "777", recursive = TRUE)
 
 # Extract training zip
 archive_extract(training_zip_file, dir = temp_dir_base)
 print("Training zip extracted successfully.")
 
 # Extract testing zip
-archive_extract(testing_zip_file, dir = temp_dir_base)
-print("Testing zip extracted successfully.")
+print("DEBUG: flag5")
+if (!is.null(testing_zip_file)) {
+    archive_extract(testing_zip_file, dir = temp_dir_base)
+    print("Testing zip extracted successfully.")
+}
+print("DEBUG: flag6")
 
 
 
@@ -94,25 +107,30 @@ print(studyid_studyids)
 
 # Read training/testing CSVs (provided directly, not from ZIP)
 training_csv <- read.csv(training_data_labels)
-testing_csv <- read.csv(testing_data_labels)
-
 cat("Loaded training CSV:\n")
 print(head(training_csv))
 
-cat("Loaded testing CSV:\n")
-print(head(testing_csv))
+if (!is.null(testing_zip_file)) {
+    testing_csv <- read.csv(testing_data_labels)
+    cat("Loaded testing CSV:\n")
+    print(head(testing_csv))
+}
 
 # Combine training and testing CSV
-combined_csv <- rbind(training_csv, testing_csv)
+if (!is.null(testing_zip_file)) {
+    combined_csv <- rbind(training_csv, testing_csv)
+} else {
+    combined_csv <- training_csv
+}
 combined_csv$STUDYID <- as.character(combined_csv$STUDYID)  # Ensure consistency
 print(head(combined_csv))
 
 #----------------------------------------------------------------------------
 
-liver_scores <- get_liver_om_lb_mi_tox_score_list (studyid_or_studyids = studyid_studyids,
+liver_scores <- get_liver_om_lb_mi_tox_score_list(studyid_or_studyids = studyid_studyids,
                                                    path_db = path_db,
                                                    fake_study = TRUE,
-                                                   use_xpt_file =  TRUE,
+                                                   use_xpt_file =  FALSE,
                                                    output_individual_scores = TRUE,
                                                    output_zscore_by_USUBJID = FALSE)
 
@@ -128,18 +146,29 @@ liver_scores_target_organ <- inner_join(combined_csv, liver_scores_col_harmonize
 training_data <- liver_scores_target_organ[!(liver_scores_target_organ$Target_Organ == "testing_data_Liver" |liver_scores_target_organ$Target_Organ ==  "testing_data_not_Liver"), ]
 
 # Replace only the target_organ column values where they are "liver" with 1
-training_data$Target_Organ[training_data$Target_Organ == "Liver"] <- 1
+training_data$Target_Organ[toupper(training_data$Target_Organ) == "LIVER"] <- 1
 
 # Replace only the target_organ column values where they are "not_liver" with 0
-training_data$Target_Organ[training_data$Target_Organ == "not_Liver"] <- 0
+training_data$Target_Organ[toupper(training_data$Target_Organ) == "NOT_LIVER"] <- 0
 
 # Convert the target_organ column to a factor with levels 1 and 0
 training_data$Target_Organ <- factor(training_data$Target_Organ, levels = c(1, 0))
 
-# creating testing data
-testing_data <- liver_scores_target_organ[(liver_scores_target_organ$Target_Organ == "testing_data_Liver" |liver_scores_target_organ$Target_Organ ==  "testing_data_not_Liver"), ]
-
 write.csv(training_data, file = paste(output_dir, "/training_data.csv", sep = ""), row.names = FALSE)
-write.csv(testing_data, file = paste(output_dir, "/testing_data.csv", sep = ""), row.names = FALSE)
+
+# creating testing data
+if (!is.null(testing_zip_file)) {
+    testing_data <- liver_scores_target_organ[(liver_scores_target_organ$Target_Organ == "testing_data_Liver" |liver_scores_target_organ$Target_Organ ==  "testing_data_not_Liver"), ]
+    # Replace only the target_organ column values where they are "liver" with 1
+    testing_data$Target_Organ[toupper(testing_data$Target_Organ) == "LIVER"] <- 1
+    testing_data$Target_Organ[toupper(testing_data$Target_Organ) == "TESTING_DATA_LIVER"] <- 1
+
+    # Replace only the target_organ column values where they are "not_liver" with 0
+    testing_data$Target_Organ[toupper(testing_data$Target_Organ) == "NOT_LIVER"] <- 0
+    testing_data$Target_Organ[toupper(testing_data$Target_Organ) == "TESTING_DATA_NOT_LIVER"] <- 0
+
+    write.csv(testing_data, file = paste(output_dir, "/testing_data.csv", sep = ""), row.names = FALSE)
+}
+
 # Print a message indicating where the files have been saved
 cat("Results have been written to", output_dir, "\n")
